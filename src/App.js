4e0713834069dcd247791f8f9529fe72d9f79852
Conor <67439130+conorkenahan@users.noreply.github.com>
2021-04-22 09:04:13 -0400

    questions pulled in from data.json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
+  state = {
+    step: 'start',
+    score: 0,
+    currentQuestion: 0,
+    questions: [],
+  }
 
-export default App;
+  componentDidMount(){
+    fetch("./data.json")
+    .then(res => res.json())
+    .then(data => this.setState({questions: data.questions}))
+  }
+  render() {
+      return (
+        <div className="App">
+          <h1>Who sang what?</h1>
+          <h2>A short quiz about the Beatles</h2>
+        </div>
+      );
+  }
+}
